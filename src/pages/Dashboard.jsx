668f9883c1bfc2f07f864b7bac416c5f5c77c937
Owl-in-night <129224909+Owl-in-night@users.noreply.github.com>
2025-02-22 22:53:50 -0600

    Second Commit

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -27,120 +27,63 @@ function Dashboard() {
 
       {/* Sección 2: Contenido del Dashboard */}
       <section className="p-6">
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            {/* Card 1 */}
-            <div className="aspect-video rounded-xl p-6 bg-slate-50 border border-gray-200 shadow-sm dark:bg-stone-800 dark:border-stone-700">
-              <Thermometer className="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" />
-              <a href="#">
-                <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                  Temperaturas
-                </h5>
-              </a>
-              <div className="mb-3 font-normal text-gray-500 dark:text-gray-400">
-                
-              </div>
-              <a
-                href="#"
-                className="inline-flex font-medium items-center text-blue-600 hover:underline"
-              >
-                Visualizar datos de temperatura
-                <SquareArrowOutUpRight className="w-4 h-4 ms-2.5 rtl:rotate-[270deg]" />
-              </a>
-            </div>
-
-            {/* Card 2 */}
-            <div className="aspect-video rounded-xl p-6 bg-slate-50 border border-gray-200 shadow-sm dark:bg-stone-800 dark:border-stone-700">
-              <Leaf className="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" />
-              <a href="#">
-                <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                  Pirólisis
-                </h5>
-              </a>
-              <div className="mb-3 font-normal text-gray-500 dark:text-gray-400">
-             
-              </div>
-              <a
-                href="https://docs.google.com/spreadsheets/d/18xQhwtCy7nI3E_c6AGkfpduV_3L84fryk87u_jQXQbk/edit?gid=1449944635#gid=1449944635"
-                className="inline-flex font-medium items-center text-blue-600 hover:underline"
-              >
-                Visualizar más datos de pirólisis
-                <SquareArrowOutUpRight className="w-4 h-4 ms-2.5 rtl:rotate-[270deg]" />
-              </a>
-            </div>
-
-            {/* Card 3 */}
-            <div className="aspect-video rounded-xl p-6 bg-slate-50 border border-gray-200 shadow-sm dark:bg-stone-800 dark:border-stone-700">
-              <BrickWall className="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" />
-              <a href="#">
-                <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                  Cantidad de colección
-                </h5>
-              </a>
-              <div className="flex">
-                <p className="mb-3 font-normal text-gray-500 dark:text-gray-400"></p>
-                <p className="mb-3 font-normal text-gray-500 dark:text-gray-400 ml-4"></p>
-              </div>
-              <a
-                href="https://docs.google.com/spreadsheets/d/1CLFeyIRNjiqyZJwyEZLn5m9u9KPYuqnus-_jADVQLKc/edit?gid=0#gid=0"
-                className="inline-flex font-medium items-center text-blue-600 hover:underline"
-              >
-                Visualizar más datos de colección
-                <SquareArrowOutUpRight className="w-4 h-4 ms-2.5 rtl:rotate-[270deg]" />
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        >
+          <div
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+            className="relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          />
+        </div>
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+            <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+              Announcing our next round of funding.{' '}
+              <a href="#" className="font-semibold text-indigo-600">
+                <span aria-hidden="true" className="absolute inset-0" />
+                Read more <span aria-hidden="true">&rarr;</span>
               </a>
             </div>
           </div>
-
-          <div className="grid auto-rows-min gap-4 md:grid-cols-2">
-            {/* Card 4 */}
-            <div className="aspect-video rounded-xl p-6 bg-slate-50 border border-gray-200 shadow-sm dark:bg-stone-800 dark:border-stone-700">
-              <ChartArea className="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" />
-              <a href="#">
-                <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                  Gráficas
-                </h5>
-              </a>
-              {/* Aquí puedes insertar tus gráficas */}
+          <div className="text-center">
+            <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
+              Data to enrich your online business
+            </h1>
+            <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
+              fugiat veniam occaecat.
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
                 href="#"
-                className="inline-flex font-medium items-center text-blue-600 hover:underline"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Más gráficas
-                <SquareArrowOutUpRight className="w-4 h-4 ms-2.5 rtl:rotate-[270deg]" />
+                Get started
               </a>
-            </div>
-
-            {/* Card 5 */}
-            <div className="aspect-video rounded-xl p-6 bg-slate-50 border border-gray-200 shadow-sm dark:bg-stone-800 dark:border-stone-700">
-              <NotebookPen className="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" />
-              <a href="#">
-                <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                  Últimas temperaturas registradas
-                </h5>
-              </a>
-              {/* Aquí puedes insertar la data adicional */}
-            </div>
-          </div>
-
-          <div className="grid auto-rows-min gap-4 md:grid-cols-1">
-            {/* Inventario */}
-            <div className="aspect-video rounded-xl p-6 bg-slate-50 border border-gray-200 shadow-sm dark:bg-stone-800 dark:border-stone-700">
-              <FileText className="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" />
-              <a href="#">
-                <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                  Inventario
-                </h5>
-              </a>
-              <a
-                href="#"
-                className="inline-flex font-medium items-center text-blue-600 hover:underline"
-              >
-                Ir a inventario
-                <SquareArrowOutUpRight className="w-4 h-4 ms-2.5 rtl:rotate-[270deg]" />
+              <a href="#" className="text-sm/6 font-semibold text-gray-900">
+                Learn more <span aria-hidden="true">→</span>
               </a>
             </div>
           </div>
         </div>
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        >
+          <div
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+            className="relative left-[calc(50%+3rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          />
+        </div>
+      </div>
       </section>
     </div>
   );
